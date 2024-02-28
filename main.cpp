6050bf69b60f82dad86fd85aff5239027fdac8de
Чумаков Даниил Камбарбекович <dkc1@tpu.ru>
2024-02-28 17:03:08 +0700

    Added gnuplot support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <random>
+#include <gsl/gsl_randist.h>
 #include "curve_fit.hpp"
+#include "gnuplot.h"
+#include <fstream>
 
 // linspace from https://github.com/Eleobert/meth/blob/master/interpolators.hpp
 template <typename Container>
@@ -27,9 +30,18 @@ double gaussian(double x, double a, double b, double c)
     return a * std::exp(-0.5 * z * z);
 }
 
+double Landau(double x, double c, double mu, double sigma)
+{
+   if (sigma <= 0) return 0;
+   double den = c * gsl_ran_landau_pdf( (x-mu)/sigma );
+   return den;
+}
 
 int main()
 {
+
+    GnuplotPipe gp;
+    
     auto device = std::random_device();
     auto gen    = std::mt19937(device());
 
@@ -45,8 +57,23 @@ int main()
         ys[i] = y + dist(gen);
     }
 
-    auto r = curve_fit(gaussian, {1.0, 0.0, 1.0}, xs, ys);
+    auto r = curve_fit(Landau, {1.0, 0.0, 1.0}, xs, ys);
 
+    auto yy = std::vector<double>(xs.size());
+    std::ofstream out;
+    out.open("data.txt", std::ios::trunc);
+    for(size_t i = 0; i < xs.size(); i++)
+    {
+        double y =  Landau(xs[i], r[0], r[1], r[2]);
+        yy[i] = y;
+        out << xs[i] << "," << ys[i] << "," << yy[i] << "\n";
+    }
+ 
+    out.close();
+    gp.sendLine("set datafile separator \",\"");
+    gp.sendLine("plot \"data.txt\" using 1:2 with lines, '' using 1:3 with lines");
+
+    std::cout << "1\n";
     std::cout << "result: " << r[0] << ' ' << r[1] << ' ' << r[2] << '\n';
     std::cout << "error : " << r[0] - a << ' ' << r[1] - b << ' ' << r[2] - c << '\n';
 
